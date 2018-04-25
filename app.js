3749d3122a6e79f07ddf233686f2166e9ff759dc
Kevin Novak <kevinmnovak@yahoo.com>
2018-04-24 22:18:58 -0400

    Basic example

---
+++ app.js
@@ -0,0 +1,14 @@
+const puppeteer = require('puppeteer');
+
+(async () => {
+    const browser = await puppeteer.launch({
+        // headless: false
+    });
+    const page = await browser.newPage();
+    await page.goto('https://example.com');
+    await page.screenshot({
+        path: 'example.png'
+    });
+
+    await browser.close();
+})();
