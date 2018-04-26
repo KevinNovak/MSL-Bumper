19a7c255010b08db4193925ca924897b8ed9aec9
Kevin Novak <kevinmnovak@yahoo.com>
2018-04-25 21:43:10 -0400

    More log statements

--- app.js
+++ app.js
@@ -9,33 +9,44 @@ const baseUrl = 'https://minecraft-server-list.com/';
 
 async function bumpServer() {
     // Open browser
+    console.log('Opening browser...');
     const browser = await puppeteer.launch({
         headless: config.hideBrowser
     });
     const page = await browser.newPage();
 
-    // Login
+    // Input username and password
+    console.log('Inputing username and password...');
     await page.goto(baseUrl + 'login/login.php');
     await page.type(`input[name='username']`, config.username);
     await page.type(`input[name='password']`, config.password);
     await delay(page);
+
+    // Login
+    console.log('Logging in...');
     await page.click(`[name='Submit']`);
     await page.waitForNavigation();
     await delay(page);
 
-    // Bump server
+    // Nagivate to "Edit Server" page
+    console.log('Navigating to "Edit Server" page...');
     await page.goto(baseUrl + `login/edit_server.php?server_id=${config.serverId}`);
     await delay(page);
+
+    // Bump server
     if (config.bumpEnabled) {
+        console.log('Bumping Server...');
         await page.evaluate(saveServer());
         await delay(page);
     }
 
     // Logout
+    console.log('Logging out...');
     await page.goto(baseUrl + 'login/logout.php');
     await delay(page);
 
     // Close browser
+    console.log('Closing browser...');
     await browser.close();
 }
 
