352d7742f4cb53ebea9f7e35ee917ba9d7e89a59
Kevin Novak <kevinmnovak@yahoo.com>
2018-04-26 20:46:15 -0400

    Added console.log for cron job start.

--- cron.js
+++ cron.js
@@ -2,6 +2,8 @@ const cron = require('cron');
 const app = require('./app');
 const config = require('./config.json');
 
+console.log('Started running script with built-in scheduler.');
+
 var job = new cron.CronJob(config.schedule.cronExpression, () => {
     console.log('You will see this message every second.');
     // app.bumpServer();
