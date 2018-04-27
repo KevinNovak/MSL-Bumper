77de18cf5a9c73845d40a38d25c61b73fa4a74fd
Kevin Novak <kevinmnovak@yahoo.com>
2018-04-26 21:08:07 -0400

    Log messages with timestamps

--- cron.js
+++ cron.js
@@ -2,9 +2,14 @@ const cron = require('cron');
 const app = require('./app');
 const config = require('./config.json');
 
-console.log('Started the built-in scheduler. Script will now run according to the configured cron expression.');
+log('Started the built-in scheduler. Script will now run according to the configured cron expression.');
 
 var job = new cron.CronJob(config.schedule.cronExpression, () => {
-    console.log('Running the script...');
+    log('Running the script...');
     // app.bumpServer();
 }, null, true, config.schedule.timezone);
+
+function log(message) {
+    const now = new Date().toLocaleString();
+    console.log(`[${now}] ${message}`);
+}
