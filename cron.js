f964ff54b201c97c8fc0e37bb9c64bd41a88d253
Kevin Novak <kevinmnovak@yahoo.com>
2018-04-26 20:53:03 -0400

    Added timezone to config, updated console logs.

--- cron.js
+++ cron.js
@@ -2,9 +2,9 @@ const cron = require('cron');
 const app = require('./app');
 const config = require('./config.json');
 
-console.log('Started running script with built-in scheduler.');
+console.log('Started the built-in scheduler. Script will now run according to the configured cron expression.');
 
 var job = new cron.CronJob(config.schedule.cronExpression, () => {
-    console.log('You will see this message every second.');
+    console.log('Running the script...');
     // app.bumpServer();
-}, null, true, 'America/Los_Angeles');
+}, null, true, config.schedule.timezone);
