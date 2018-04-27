95d5639b3abdb4aa9cd5e8f39c062bb3ed328d46
Kevin Novak <kevinmnovak@yahoo.com>
2018-04-26 20:42:17 -0400

    Use the cron expression from the config

--- cron.js
+++ cron.js
@@ -1,7 +1,8 @@
 const cron = require('cron');
 const app = require('./app');
+const config = require('./config.json');
 
-var job = new cron.CronJob('* * * * * *', () => {
+var job = new cron.CronJob(config.schedule.cronExpression, () => {
     console.log('You will see this message every second.');
     // app.bumpServer();
 }, null, true, 'America/Los_Angeles');
